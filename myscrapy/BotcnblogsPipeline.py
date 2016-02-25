80855570a6270eb632c805d2d19d934ff4e4809a
zhckaka@sina.com <zhckaka@sina.com>
2016-02-25 15:19:05 +0800

    修改一个spider中解析多个item并在pipeline中判断处理

--- myscrapy/BotcnblogsPipeline.py
+++ myscrapy/BotcnblogsPipeline.py
@@ -10,13 +10,24 @@ class BotcnblogsPipeline(object):
     
     def __init__(self):
         self.file=open('jd.json','w+')
-#         self.json=open('it.json','w+')
+        self.json=open('it.json','w+')
+        
     @check_spider_pipeline
     def process_item(self,item,spider):
+        print("item的类型是: %s" %(type(item),))
         #此处如果有中文的话，要加上ensure_ascii=False参数，否则可能出现乱码
-        record=json.dumps(dict(item),ensure_ascii=False)+"\n" 
-        self.file.write(record)
-        return item
+        vl=item.get('titles','None')
+        if  vl is not 'None':
+            print(u"真的可以啊")
+            ft=json.dumps(dict(item),ensure_ascii=False)+"\n" 
+            self.json.write(ft)
+            return item
+        else:
+            record=json.dumps(dict(item),ensure_ascii=False)+"\n" 
+            self.file.write(record)
+            return item
+            
+        
     
     def open_spider(self,spider):
         print("打开爬虫了")
